649b9f18a6d63054626a78e3b755586ffe96cbcb
garufi847-lgtm <garufi847@gmail.com>
2026-03-31 12:23:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,4 @@
-const C = 'pres-v2';
-self.addEventListener('install', e => e.waitUntil(
-  caches.open(C).then(c => c.add('./')).then(() => self.skipWaiting())
-));
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys()
-    .then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-));
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
-));
+const C='pres-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.add('./index.html')).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
